f60902605f4d51af9358c396a6d9b0ee733ae8c2
Ziyang Zhang <ziz361@ucsd.edu>
2019-11-04 11:51:50 -0800

    changed output

--- consensus_matrix/threshold_calculator.py
+++ consensus_matrix/threshold_calculator.py
@@ -5,7 +5,8 @@ import numpy as np
 
 path = "/mnt/silencer2/home/ziz361/data/consensus_matrix_data/cs_random1.csv"
 percentile = 0.001
-image_path = "/mnt/silencer2/home/ziz361/data/consensus_matrix_data/cs_random1_plot.csv"
+image_path = "/mnt/silencer2/home/ziz361/data/consensus_matrix_data/cs_random1_plot.png"
+output_path = "/mnt/silencer2/home/ziz361/data/consensus_matrix_data/cs_random1_threshold.txt"
 
 # remove redundant values from the consensus matrix as it is symmetric 
 # flatten data afterwards 
@@ -18,17 +19,21 @@ def df_preprocess(df):
     flattened_data = flattened_data[flattened_data!=0]
     print("Total data points: {}".format(len(flattened_data)))
     return flattened_data
-
+print("Preprocessing Data")
 data = genfromtxt(path, delimiter = ',')
 df = pd.DataFrame(data)
 df.values[tuple([np.arange(df.shape[0])]*2)] = 0 #set diaganol value to 0 for removal
 cs_data = df_preprocess(df)
 
+print("Making histograms")
 _ = plt.hist(cs_data, bins = "auto")
 plt.title("Histogram for consensus matrix")
 plt.xlabel("Normalized consensus count")
 plt.ylabel("Frequency")
 plt.savefig(image_path)
+
+print("calculating threshold")
 cs_data_np = np.array(cs_data)
 threshold = np.percentile(cs_data_np, percentile)
-print("Calculated threshold is: {}".format(threshold))
+with open(output_path, "w") as text_file:
+    print("Purchase Amount: {}".format(threshold), file=text_file)
